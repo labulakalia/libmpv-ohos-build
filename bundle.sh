d3b8348c5b9c5e86d57fea0acbcc729b82965b9e
labulakalia <labulakalia@labulakalia.labulakalia>
2026-09-08 09:38:39 -0400

    fix build failed

--- bundle.sh
+++ bundle.sh
@@ -2,13 +2,13 @@
 
 set -exu
 
-sh -x ./download.sh
+./download.sh
 ./patch.sh
 
 export ARCH=amd64
 mkdir -p ./libmpv/amd64-build
 cp ./crossfiles/amd64-crossfile-linux.ini ./libmpv/crossfile.ini
-sh -x ./build.sh amd64
+./build.sh amd64
 
 (
     cd ./libmpv/amd64-build
@@ -18,10 +18,9 @@ sh -x ./build.sh amd64
 export ARCH=arm64
 mkdir -p ./libmpv/arm64-build
 cp ./crossfiles/arm64-crossfile-linux.ini ./libmpv/crossfile.ini
-sh -x ./build.sh arm64
+./build.sh arm64
 
 (
     cd ./libmpv/arm64-build
     zip libmpv_aarch64.zip libmpv.so
 )
-
--- download.sh
+++ download.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
-
 set -exu
 
-sh -x ./download/download-sdk.sh
-# if [ "$(uname -s)" = "Linux" ]; then
-#   if [ ! -d /sdk ]; then
-#     echo "Downloading OpenHarmony SDK..."
-#     ./download/download-sdk.sh
-#     ln -sf ../crossfiles/${arch}-crossfile-linux.ini ./libmpv/${arch}-crossfile.ini
-#   fi
-# elif [ "$(uname -s)" = "Darwin" ]; then
-#   echo "Using DevEco Studio for macOS, please make sure DevEco Studio is installed."
-#   ln -sf ../crossfiles/arm64-crossfile-macos.ini ./libmpv/arm64-crossfile.ini
-# else
-#   echo "Unsupported platform." >&2
-#   exit 1
-# fi
-
-sh -x ./download/download-ohos-rs.sh
-sh -x ./download/download-deps.sh
+./download/download-sdk.sh
+./download/download-ohos-rs.sh
+./download/download-deps.sh
